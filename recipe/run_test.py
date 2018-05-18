af989e9e0ca8276e5a7d9f2d79317a4711e1fbb2
Mark Harfouche <mark.harfouche@gmail.com>
2018-05-17 20:01:15 -0700

    Rebuilt for numpy 1.14 and scikit image 0.13

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,7 +1,16 @@
 import sys
 import matplotlib
+import numpy as np
+from distutils.version import LooseVersion as Version
 
 matplotlib.use('Agg')
+
+# Use legacy numpy printing. This fix is made to keep doctests functional.
+# For more info, see https://github.com/scikit-image/scikit-image/pull/2935 .
+# TODO: remove this workaround once minimal required numpy is set to 1.14.0
+
+if Version(np.__version__) >= Version('1.14'):
+     np.set_printoptions(legacy='1.13')
 
 import skimage
 
