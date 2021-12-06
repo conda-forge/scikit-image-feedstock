14f86b3fb53c605bbb4c4c918d90197fd2386a69
Eli Rykoff <erykoff@stanford.edu>
2021-12-06 07:45:34 -0800

    Change to make clean and add patch to avoid conversion overflows.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,5 @@
 set -ex
 
-rm -f skimage/filters/*.c
-rm -f skimage/restoration/_*.c
-rm -f skimage/segmentation/*.c
-rm -f skimage/io/*.c
-rm -f skimage/graph/*.c
-rm -f skimage/measure/*.c
-rm -f skimage/_shared/*.c
-rm -f skimage/morphology/*.c
-rm -f skimage/feature/*.c
-rm -f skimage/transform/*.c
-rm -f skimage/draw/*.c
-rm -f skimage/future/*.c
+make clean
 
 ${PYTHON} -m pip install . -vv
