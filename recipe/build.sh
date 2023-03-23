5388579232740b899fbbcf4c5e697486aa67c5df
Stefan van der Walt <stefanv@berkeley.edu>
2023-03-22 17:46:01 -0700

    Setup meson with cross-compile flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 set -ex
 # clean any cython-generated .c/.cpp files (as used by "make clean")
 find . -name "*.pyx" -exec ./tools/rm_pyx_assoc_c_cpp.sh {} \;
+meson setup ${MESON_ARGS} builddir/
 ${PYTHON} -m pip install . -vv
