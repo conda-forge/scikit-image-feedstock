1c94825479f4021b0101ef7359a778ab3cf2114d
Mark Harfouche <mark.harfouche@gmail.com>
2023-03-29 08:45:18 -0400

    copy build process from scikit image

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,12 @@
 set -ex
 # clean any cython-generated .c/.cpp files (as used by "make clean")
 find . -name "*.pyx" -exec ./tools/rm_pyx_assoc_c_cpp.sh {} \;
-meson setup ${MESON_ARGS} builddir/
-${PYTHON} -m pip install . -vv
+
+mkdir builddir
+
+# need to run meson first for cross-compilation case
+${PYTHON} $(which meson) setup ${MESON_ARGS} \
+    builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+${PYTHON} -m pip install dist/scikit-image*.whl
