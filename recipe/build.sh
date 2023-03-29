2ef040ae2f77bdb7b88b7a2d9e5328fdf2d000e7
Stefan van der Walt <stefanv@berkeley.edu>
2023-03-28 22:26:02 -0700

    Invoke correct Python when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 set -ex
 # clean any cython-generated .c/.cpp files (as used by "make clean")
 find . -name "*.pyx" -exec ./tools/rm_pyx_assoc_c_cpp.sh {} \;
-meson setup ${MESON_ARGS} builddir/
-${PYTHON} -m pip install . -vv
+meson setup ${MESON_ARGS} builddir/ || (cat builddir/meson-logs/meson-log.txt && exit 1)
+$PYTHON -m build -w -n -x -Cbuilddir=builddir
+pip install dist/scikit-image*.whl
